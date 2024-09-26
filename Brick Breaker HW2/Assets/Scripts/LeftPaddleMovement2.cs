eb26a588b1bb41a40d4a843eca78fd987c13d5a9
DeshuiY <148656746+DeshuiY@users.noreply.github.com>
2024-09-26 06:37:44 -0600

    UpDate

---
+++ Brick Breaker HW2/Assets/Scripts/LeftPaddleMovement2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPaddleMovement : MonoBehaviour
+{
+    
+    
+        public float speed = 10f;
+        public float boundaryX = 7.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontal, 0f, 0f)*speed*Time.deltaTime;
+        transform.position += movement;
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float horizontal = Input.GetAxis("Horizontal");
+       Vector3 movement = new Vector3(horizontal, 0f, 0f)*speed*Time.deltaTime;
+       transform.position += movement;
+       
+       float clampedX = Mathf.Clamp(transform.position.x, -boundaryX, boundaryX);
+       transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+}
